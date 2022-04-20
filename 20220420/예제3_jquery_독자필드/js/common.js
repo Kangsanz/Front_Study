5a02758f48de8f3d0b22dd32ddd733db9484b26b
Kangsanz <xmrtnanswk80@gmail.com>
2022-04-20 16:31:15 +0900

    20220420

--- 20220420/예제3_jquery_독자필드/js/common.js
+++ 20220420/예제3_jquery_독자필드/js/common.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
   //jquery 실행구문
   // first, first-child / last, last-child 비교 예제
   // $('p:first-child').css('color','red');
@@ -23,6 +23,38 @@ $(function(){
   // 해당 인덱스와 같은 위치의 요소를 선택
   // $('li:gt(2)').css('color','blue');
   // eq 기준으로 해당 index 값보다 큰 요소를 선택
-  $('li:lt(2)').css('color','green');
+  // $('li:lt(2)').css('color','green');
   // eq 기준으로 해당 index 값보다 작은 요소를 선택
+
+  // heading 태그 선택
+  // $('h6:header').css('color','red');
+  // $('h1,h6').css('color','blue');
+
+  // contains 문자열로 포함 유무 확인 후 요소 선택
+  // $('li:contains("menu1")').css('color','red');
+  // $('li:contains("menu3")').css('color','green').css('font-size','20px');
+  // $('li:contains("menu5")').css('font-weight','bold');
+  // $('p:contains("!")').css('color','hotpink').css('font-weight','bold').css('font-size','20px');
+  // $('p:contains("t")').css('color','yellowgreen').css('font-weight','bold');
+  // $('p:contains("포")').css('color','purple').css('font-weight','700');
+  // let elem = $('li:contains("menu1")');
+  // if ( elem.length > 0) {
+  //   console.log('menu1 존재합니다.');
+  // }
+
+  // has 태그 포함 유무로 요소 선택
+  // $('li:has("strong")').css('color','red');
+
+  /*
+  1. p 태그를 갖고 있는 li 텍스트 색상을 blue로 변경
+  2. span 태그를 갖고 있는 li 텍스트 색상을 orange으로 변경
+  3. a 태그를 갖고 있는 li 텍스트 색상을 green으로 변경
+  */
+  // $('li:has("p")').css('color','blue');
+  // $('li:has("span")').css('color','orange');
+  // $('li:has("a"), li:has("a") a').css('color','green');
+
+  $('li:parent').css('border','1px solid red')
+  //부정유사 클래스를 사용하여 자식 요소가 존재하지 않는 요소를 선택할 수 있다.
+  $('li:not(:parent)').css('border','1px solid yellow')
 });
